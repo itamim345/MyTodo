b22691f397894c617280bba1db4393fea779f199
AF Tamim <itamim345@gmail.com>
2022-12-28 22:05:11 +0530

    Added status code in getTodosController

--- server/controllers/getTodosController.js
+++ server/controllers/getTodosController.js
@@ -2,5 +2,5 @@ import Todo from "../models/todo";
 
 export async function getTodosController(req, res){
     const allTodos = await Todo.find();
-    res.json(allTodos);
+    res.status(201).json(allTodos);
 }
