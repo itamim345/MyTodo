8f0c73667682d7831fa8298cbbe8c3aca2b8c3c3
AF Tamim <itamim345@gmail.com>
2022-12-28 22:04:19 +0530

    Added response code in getTodoController

--- server/controllers/getTodoController.js
+++ server/controllers/getTodoController.js
@@ -3,5 +3,5 @@ import Todo from "../models/todo";
 export async function getTodoController(req, res){
     const {todoId} = req.params;
     const searchedTodo = await Todo.findById(todoId);
-    res.json(searchedTodo)
+    res.status(201).json(searchedTodo)
 }
